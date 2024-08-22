1a17b0aedf1e61ef86cd2ed4d81eccb777af5342
heyabrar <heyabrarkhan@gmail.com>
2024-08-22 18:21:46 +0530

    add: React Quill editor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import QuillEditor from "../componets/Editor";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-[40px] text-center font-bold">Landing Page</h1>
+      <QuillEditor />
     </div>
   );
 }
